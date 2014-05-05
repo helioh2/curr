9343016a484852103d9537547f85afc452c9c462
Kathi Fisler <kfisler@cs.wpi.edu>
2014-05-05 11:49:27 -0400

    pyret conversion supports examples that span multiple lines

--- lib/racket2pyret.rkt
+++ lib/racket2pyret.rkt
@@ -33,9 +33,12 @@
                                 (lookup-binop (first sexp))
                                 (format-pyret-arglist (rest sexp)))]
              [(EXAMPLE)
-              (format "~a is ~a" 
-                      (format-help (second sexp))
-                      (format-help (third sexp)))]
+              (let ([fmtstr (if multi-line?
+                                "~a is~n~a"
+                                "~a is ~a")])
+                (format fmtstr
+                        (format-help (second sexp))
+                        (format-help (third sexp))))]
              [(define) 
               (cond [(atom? (second sexp)) ; defining a constant
                      (if (string? (third sexp))
@@ -94,20 +97,28 @@
   (and (> (string-length str) 8)
        (string=? "(EXAMPLE" (substring str 0 8))))
 
-(define (format-example-line bs1examplestr)
-  (format-bs1-as-pyret (with-input-from-string bs1examplestr read)))
+(define (format-example-line bs1examplestr #:multi-line? (multi-line? #f))
+  (printf "formatting example ~a~n~n" bs1examplestr)
+  (printf "reads as ~s~n~n" (with-input-from-string bs1examplestr read))
+  (format-bs1-as-pyret (with-input-from-string bs1examplestr read) #:multi-line? multi-line?))
    
 ;; from-lines is a list of strings corresponding to a block of code
-;; assumes no example extends across more than two lines
-;(define (read-example from-lines)
-;  (if (racket-example? (first from-lines))
-;      (let ([oneline (format-oneline-bs1-as-pyret (first from-lines))])
-;        (if oneline oneline
-;            (let loop ([curr-example (list (first from-lines) (second from-lines))] 
-;                       [all-lines (rest (rest from-lines))])
-;              #t)))))
-              
-
+;; iterates until finding smallest number of lines that read as an example
+;; returns as values the converted example string and how many lines were used
+;;   to create the example
+(define (read-example from-lines)
+  (let loop ([try-example-str (first from-lines)] [num-used-lines 1] [rem-lines (rest from-lines)])
+    (let ([complete-eg (if (= num-used-lines 1)
+                           (format-oneline-bs1-as-pyret try-example-str)
+                           (format-manyline-bs1-as-pyret (list try-example-str)))])
+      (if complete-eg 
+          (values complete-eg num-used-lines)
+          (if (empty? rem-lines)
+              (error 'read-example "Never found complete example in ~s~n" from-lines)
+              (loop (string-append try-example-str " " (first rem-lines))
+                    (add1 num-used-lines)
+                    (rest rem-lines)))))))
+           
 ;;;;;;;;; exported API for rendering code ;;;;;;;;;;;;;;;;;
 
 ;; consumes string or sexp of bs1 code and produces string of pyret code or #f
@@ -124,17 +135,14 @@
                  (if (string? bs1str-or-sexp) (with-input-from-string bs1str-or-sexp read) bs1str-or-sexp)
                  #:multi-line? #f)])))
 
-;; takes list of strings that make up one racket expression (optionally with leading comments)
-;;   and produce pyret rendering as a string
-;; assumes comment lines, if any, are at the top of the code segment
-;; assumes example lines, if any, are between comments and code
+;; takes list of strings that make up one racket expression (optionally with leading comments/examples)
+;;   and produces pyret rendering as a string
+;; assumes comment and example lines, if any, are at the top of the code segment
 ;; - input from scribble comes in with newlines -- we filter those out at the start of the loop
 ;;   and reinsert them post conversion
 ;; MUST FIX to support examples that extend over multiple lines
 (define (format-manyline-bs1-as-pyret bs1strs)
-  (with-handlers ([exn:fail:read? (lambda (exn) 
-                                    (printf "Multiline failed to parse: ~a~n" bs1strs)
-                                    "!!!!KATHI FIX ME!!!!!")]
+  (with-handlers ([exn:fail:read? (lambda (exn) #f)]
                   [(lambda (exn) #t)
                    (lambda (exn) 
                      (printf "EXN msg: ~s~n" (exn-message exn))
@@ -145,7 +153,10 @@
           (cond [(racket-comment? (first all-lines))
                  (loop (rest all-lines) (cons (format-comment-line (first all-lines)) comment-lines) example-lines)]
                 [(racket-example? (first all-lines))
-                 (loop (rest all-lines) comment-lines (cons (format-example-line (first all-lines)) example-lines))]
+                 (let-values ([(examplestr used-lines) (read-example all-lines)])
+                   (loop (list-tail all-lines used-lines)
+                         comment-lines 
+                         (cons examplestr example-lines)))]
                 [else ;; should only be body lines left
                  (let* ([pylines (map format-oneline-bs1-as-pyret all-lines)]
                         [body-lines
